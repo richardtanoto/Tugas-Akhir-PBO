73e11e63c3c1f795a1477797e28bef3441723810
Elvia <elviaclarissa2002@gmail.com>
2021-07-06 19:41:42 +0700

    Create Tstok.java

---
+++ Tracker/src/Tstok.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Tstok extends Barang{
+    ArrayList<Barang> barangs = new ArrayList<Barang>();
+
+    static int jumlah ;
+    
+    void tambah(Barang b){
+        barangs.add(b);
+    }
+    void menambahbarang(Barang b){
+        System.out.println(" Menambah Barang ");
+        tambah(b);
+        System.out.println(b.toString());
+        
+        System.out.println("Data Berhasil Ditambahkan !!!");
+    }
+
+    void tampilkan(){
+        System.out.println("No" + "\t\t" + "Kode Barang" + "\t\t\t" + "Nama Barang" + "\t\t" + "Jumlah Barang" + "\t\t\t" + "Harga Barang" + "\t" );
+        int i = 1;
+        for (Barang barang : barangs) {
+            System.out.print(i);
+            System.out.println(barang.databrg());
+            i+=1;
+        }
+    }
+}
