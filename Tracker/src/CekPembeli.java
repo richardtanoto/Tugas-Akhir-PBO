f08b0688f49dbe950ff74a226ed9832557b9cc69
SriRahayu2 <srirahayu.lin2@gmail.com>
2021-07-08 18:20:48 +0700

    Update CekPembeli.java

--- Tracker/src/CekPembeli.java
+++ Tracker/src/CekPembeli.java
@@ -2,10 +2,16 @@ import java.util.ArrayList;
 public class CekPembeli extends Pelanggan {
     ArrayList<Pelanggan> customer = new ArrayList<Pelanggan>();
 
+    
     void cekcustomer(Pelanggan p){
         customer.add(p);
     }    
-
+    
+    void cekcustomer1(Pelanggan p){
+        customer.add(p);
+        System.out.println("Data Berhasil Ditambahkan!");
+        System.out.println(p.tampil());
+    }    
 
     public void tampilkan(){
         System.out.println("No" + "\t\t" + "ID NUMBER" + "\t\t" + "Nama" + "\t\t" + "Jenis Kelamin" + "\t\t" + "No.Telp" + "\t\t" +
@@ -18,3 +24,4 @@ public class CekPembeli extends Pelanggan {
         }      
     }
 }
+
